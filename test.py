482224737ba4d5b117ae3e6837757042ae172955
Abcfsa <1216841430@qq.com>
2023-09-03 13:15:27 +0800

    opencv problem solved

--- test.py
+++ test.py
@@ -15,8 +15,32 @@ from utils.general import (LOGGER, check_file, check_img_size, check_imshow, che
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 from utils.torch_utils import select_device, time_sync
 from utils.augmentations import Albumentations, augment_hsv, copy_paste, letterbox, mixup, random_perspective
-def click():
+def click(choice,data,cur):
     sl.session_state.clicked=True
+    if choice=="kid":
+        if data.shape[0]>1:
+            re_ids=data[data['id']!=cur]
+            for id_ind in re_ids.index:
+                matrix=(sl.session_state.df['id']==re_ids.loc[id_ind]['id']) & (sl.session_state.df['frame']>=re_ids.loc[id_ind]['frame']) & (sl.session_state.label['id']=='kid') 
+                if 'teacher' in data['id'].map(str).unique():
+                    sl.session_state.label.loc[matrix,'id']='neither'
+                else:
+                    sl.session_state.label.loc[matrix,'id']='teacher'
+        sl.session_state.label.loc[sl.session_state.df['id']==cur,'id']='kid'
+    elif choice=="tea":
+        if data.shape[0]>1:
+            re_ids=data[data['id']!=cur]
+            for id_ind in re_ids.index:
+                matrix=(sl.session_state.df['id']==re_ids.loc[id_ind]['id']) & (sl.session_state.df['frame']>=re_ids.loc[id_ind]['frame']) & (sl.session_state.label['id']=='teacher') 
+                if 'kid' in data['id'].map(str).unique():
+                    sl.session_state.label.loc[matrix,'id']='neither'
+                else:
+                    sl.session_state.label.loc[matrix,'id']='kid'
+        sl.session_state.label.loc[sl.session_state.df['id']==cur,'id']='teacher'
+    elif choice=="nei":
+        sl.session_state.label.loc[sl.session_state.df['id']==cur,'id']='neither'
+    # elif choice=="quit":
+    #     sys.exit(0)
 # @sl.cache_data
 def head_detect(_tracker,f_num,df,img0,_model,_device, 
     imgsz,conf_thres=0.25,
@@ -50,96 +74,117 @@ def head_detect(_tracker,f_num,df,img0,_model,_device,
     for normal, id, conf, cls in zip(normals, ids, confs, clss):
         new=[f_num,normal[0],normal[1],normal[2],normal[3],id]
         df.loc[df.shape[0]]=new
+
 if 'clicked' not in sl.session_state:
     sl.session_state.clicked=False
+    sl.session_state.file=None
+
 sl.sidebar.header("Gaze Follow")
 sl.header("Gaze Follow")
 file=sl.file_uploader("Video",type=['mov', 'avi', 'mp4', 'mpg', 'mpeg', 'm4v', 'wmv', 'mkv'])
 if file==None:
     sl.write("Please upload a video.")
+    sl.session_state.clicked=False
 else:
+    # sl.write(file.name)
+
+    if file!=sl.session_state.file or file.name!=sl.session_state.file.name:
+        sl.session_state.clicked=False
+    sl.session_state.file=file
     tfile=tempfile.NamedTemporaryFile(delete=False)
     tfile.write(file.read())
 
     sl.video(file, format='video/mp4', start_time=0)
 
-    vid = cv2.VideoCapture(tfile.name)
-    fps = vid.get(cv2.CAP_PROP_FPS)
-    width=int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height=int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fourcc=int(vid.get(cv2.CAP_PROP_FOURCC))
-    frame_num=int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
-    # print(frame_num)
-    frame_bar=sl.progress(0,text="Progressing frame 0 out of %d"%frame_num)
-    cur_frame=0
-    frame_list = []
-    if (vid.isOpened()==False):
-        sl.write("Something Wrong occured while reading the video.")
-    while (vid.isOpened()):
-        success, frame = vid.read()
-        if success:
-            frame_list.append(frame)
-            cur_frame+=1
-            frame_bar.progress(cur_frame/frame_num,text="Progressing frame %d out of %d"%(cur_frame,frame_num))  
-        else:
-            break
-    vid.release()
-    sl.write('Frame List Created.')
-    # sl.write(frame_list[0])
-    sl.divider()
-    tracker = DeepOCSORT(
-    model_weights=Path('osnet_x0_25_msmt17.pt'), # which ReID model to use
-    device='cuda:0',
-    fp16=True,
-)   
-    head_model = 'head_detector_best.pt'
-    model, device, imgsz = detect.load_model(head_model)
-    t3 = time_sync()
+    if sl.session_state.clicked==False:
+        vid = cv2.VideoCapture(tfile.name)
+        fps = vid.get(cv2.CAP_PROP_FPS)
+        width=int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height=int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fourcc=int(vid.get(cv2.CAP_PROP_FOURCC))
+        frame_num=int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+        sl.session_state.fps=fps
+        sl.session_state.width=width
+        sl.session_state.height=height
+        sl.session_state.fourcc=fourcc
 
-    print_every=500 # print info
-    # placeholder=sl.empty()
-    df=pd.DataFrame(data=None,columns=['frame','xmin','ymin','xmax','ymax','id'])
-    d_prog=sl.progress(0)
-    for i in range(frame_num):
-        img0=frame_list[i]
-        new=head_detect(_tracker=tracker,df=df,f_num=i+1,img0=img0,_model=model,
-                                                    _device=device,imgsz=imgsz)
-        d_prog.progress((i+1)/frame_num,text="Detecting heads from frame %d"%(i+1))
-        # placeholder.image(img0,"Frame %d/%d"%(i+1,frame_num),channels="BGR")
-        # if (i+1)%print_every==0:
-        #     placeholder.write(f'Finished Processing {(i+1):d}/{frame_num:d} ({time_sync()-t3:.3f}s)')
-    sl.write("Head detection done.")
-    sl.divider()
+        # print(frame_num)
+        frame_bar=sl.progress(0,text="Progressing frame 0 out of %d"%frame_num)
+        cur_frame=0
+        frame_list = []
+        if (vid.isOpened()==False):
+            sl.write("Something Wrong occured while reading the video.")
+        while (vid.isOpened()):
+            success, frame = vid.read()
+            if success:
+                frame_list.append(frame)
+                cur_frame+=1
+                frame_bar.progress(cur_frame/frame_num,text="Progressing frame %d out of %d"%(cur_frame,frame_num))  
+            else:
+                break
+        vid.release()
+        sl.session_state.frame_list=frame_list
+        sl.write('Frame List Created.')
+        # sl.write(frame_list[0])
+        sl.divider()
+        tracker = DeepOCSORT(
+        model_weights=Path('osnet_x0_25_msmt17.pt'), # which ReID model to use
+        device='cuda:0',
+        fp16=True,
+        )   
+        head_model = 'head_detector_best.pt'
+        model, device, imgsz = detect.load_model(head_model)
+        t3 = time_sync()
 
-    skip=False
-    df['id']=df['id'].map(int)
-    label=df
-    recorded=['kid','teacher','neither']
-    for ind in range(df.shape[0]):
-        if skip:
-            continue
-        if label.iloc[ind]['id'] not in recorded:
-            recorded.append(label.iloc[ind]['id'])
-            frame_ind=int(label.iloc[ind]['frame'])
-            img=frame_list[frame_ind-1].copy()
+        print_every=500 # print info
+        # placeholder=sl.empty()
+        df=pd.DataFrame(data=None,columns=['frame','xmin','ymin','xmax','ymax','id'])
+        d_prog=sl.progress(0)
+        for i in range(frame_num):
+            img0=frame_list[i]
+            new=head_detect(_tracker=tracker,df=df,f_num=i+1,img0=img0,_model=model,
+                                                        _device=device,imgsz=imgsz)
+            d_prog.progress((i+1)/frame_num,text="Detecting heads from frame %d"%(i+1))
+            # placeholder.image(img0,"Frame %d/%d"%(i+1,frame_num),channels="BGR")
+            # if (i+1)%print_every==0:
+            #     placeholder.write(f'Finished Processing {(i+1):d}/{frame_num:d} ({time_sync()-t3:.3f}s)')
+        sl.write("Head detection done.")
+        sl.divider()
+
+        df['id']=df['id'].map(int)
+        sl.session_state.df=df
+        label=df
+        sl.session_state.label=label
+        recorded=['kid','teacher','neither']
+        sl.session_state.recorded=recorded
+
+        remaining=[item for item in sl.session_state.df['id'].unique() if item not in sl.session_state.recorded]
+        # sl.write(remaining)
+        if len(remaining)>0:
+            cur=remaining[0]
+            sl.session_state.recorded.append(cur)
+            filtered=sl.session_state.label[sl.session_state.label['id']==cur]
+            # sl.write(filtered)
+            frame_ind=int(filtered.iloc[0]['frame'])
+            img=sl.session_state.frame_list[frame_ind-1].copy()
             w,h=img.shape[1],img.shape[0]
-            data=df[df['frame']==frame_ind]
+            data=sl.session_state.df[sl.session_state.df['frame']==frame_ind]
+            # sl.write(data)
             if data.shape[0]!=0:
-                l_data=label[df['frame']==frame_ind] 
+                l_data=sl.session_state.label[sl.session_state.df['frame']==frame_ind] 
                 if data.shape[0]>2 and 'kid' in l_data['id'].unique() and 'teacher' in l_data['id'].unique():
-                    label.loc[label['id']==label.iloc[ind]['id'],'id']='neither'
-                    continue
+                    sl.session_state.label.loc[sl.session_state.label['id']==cur,'id']='neither'
             for num in data.index:
                 [xm,ym,xM,yM]=[int(data['xmin'][num]*w),int(data['ymin'][num]*h),
-                               int(data['xmax'][num]*w),int(data['ymax'][num]*h)]
+                            int(data['xmax'][num]*w),int(data['ymax'][num]*h)]
                 cv2.rectangle(img,
                         (xm,ym),
                         (xM,yM),
                         (0,255,0),2)
-                if num==ind:
+                if cur==data.loc[num]['id']:
                     cv2.putText(img,
-                    'id:%d'%data['id'][num],
-                    (xm,yM+50),
+                    'id:%d'%cur,
+                    (xm,yM+35),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     1.5,(0,255,0),3)
             cv2.putText(img,
@@ -147,75 +192,100 @@ else:
                     (0,50),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     1.5,(0,0,255),3)
-            
+            sl.write("Is the headbox with id:%d the kid,the teacher or neither?"%cur)
+            sl.image(img,channels="BGR")
+            col1,col2,col3=sl.columns(3)
+            kid=col1.button("Kid",key="kid%d"%cur,on_click=click,args=['kid',data,cur])
+            tea=col2.button("Teacher",key="tea%d"%cur,on_click=click,args=['tea',data,cur])
+            nei=col3.button("Neither",key="nei%d"%cur,on_click=click,args=['nei',data,cur])
+            # quit=col4.button("Quit",key="quit%d"%cur,on_click=click,args=['quit',data,cur])
+        else:
+            sl.write("Labelling done.")
+            sl.divider()
+            out=cv2.VideoWriter('out.mp4',sl.session_state.fourcc,sl.session_state.fps,(sl.session_state.width,
+                                                                                        sl.session_state.height))
+            for i in range(len(sl.session_state.frame_list)):
+                img=sl.session_state.frame_list[i]
+                w,h=img.shape[1],img.shape[0]
+                frame_ind=i+1
+                frame_anno=sl.session_state.label[sl.session_state.label['frame']==frame_ind]
+                for num in frame_anno.index:
+                    [xm,ym,xM,yM]=[int(frame_anno['xmin'][num]*w),int(frame_anno['ymin'][num]*h),
+                                int(frame_anno['xmax'][num]*w),int(frame_anno['ymax'][num]*h)]
+                    cv2.rectangle(img,
+                            (xm,ym),
+                            (xM,yM),
+                            (0,255,0),2)
+                    cv2.putText(img,
+                        '%s'%frame_anno['id'][num],
+                        (xm,yM+25),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,(0,255,0),2)
+                out.write(img)
+            out.release()
+            sl.video('out.mp4', format='video/mp4', start_time=0)
+    else:
+        remaining=[item for item in sl.session_state.df['id'].unique() if item not in sl.session_state.recorded]
+        # sl.write(remaining)
+        if len(remaining)>0:
+            cur=remaining[0]
+            sl.session_state.recorded.append(cur)
+            filtered=sl.session_state.label[sl.session_state.label['id']==cur]
+            frame_ind=int(filtered.iloc[0]['frame'])
+            img=sl.session_state.frame_list[frame_ind-1].copy()
+            w,h=img.shape[1],img.shape[0]
+            data=sl.session_state.df[sl.session_state.df['frame']==frame_ind]
+            if data.shape[0]!=0:
+                l_data=sl.session_state.label[sl.session_state.df['frame']==frame_ind] 
+                if data.shape[0]>2 and 'kid' in l_data['id'].unique() and 'teacher' in l_data['id'].unique():
+                    sl.session_state.label.loc[sl.session_state.label['id']==cur,'id']='neither'
+            for num in data.index:
+                [xm,ym,xM,yM]=[int(data['xmin'][num]*w),int(data['ymin'][num]*h),
+                            int(data['xmax'][num]*w),int(data['ymax'][num]*h)]
+                cv2.rectangle(img,
+                        (xm,ym),
+                        (xM,yM),
+                        (0,255,0),2)
+                if cur==data.loc[num]['id']:
+                    cv2.putText(img,
+                    'id:%d'%cur,
+                    (xm,yM+35),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1.5,(0,255,0),3)
             cv2.putText(img,
-                            "Is the headbox with id:%d the kid,the teacher or neither"%label.iloc[ind]['id'],
-                            (0,img.shape[0]-80),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1,(255,255,255),2)
-            cv2.putText(img,
-                            "Type k if kid,t if teacher,n if neither,q to quit,s to skip",
-                            (0,img.shape[0]-30),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1,(255,255,255),2)
-            cv2.namedWindow('img',cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('img', 800, 600)
-            time.sleep(0.01)
-            #print("Is the headbox with id:%d the kid,the teacher or neither?\nType k if kid,t if teacher,n if neither,q to quit: "%label.iloc[num]['id'])
-            cv2.imshow('img',img)
-            order=-1
-            while order not in [ord('q'),ord('k'),ord('t'),ord('n'),ord('s')]:
-                order=cv2.waitKey(0) & 0xFF
-            if order==ord('q'):
-                sys.exit(0)
-            elif order==ord('k'):
-                if data.shape[0]>1:
-                    re_ids=data[data['id']!=label.iloc[ind]['id']]
-                    for id_ind in re_ids.index:
-                        matrix= (df['id']==re_ids.loc[id_ind]['id']) & (df['frame']>=re_ids.loc[id_ind]['frame']) & (label['id']=='kid') 
-                        if 'teacher' in data['id'].map(str).unique():
-                            label.loc[matrix,'id']='neither'
-                        else:
-                            label.loc[matrix,'id']='teacher'
-                label.loc[df['id']==label.iloc[ind]['id'],'id']='kid'
-                #print(label[:50])
-            elif order==ord('t'):
-                if data.shape[0]>1:
-                    re_ids=data[data['id']!=label.iloc[ind]['id']]
-                    for id_ind in re_ids.index:
-                        matrix= (df['id']==re_ids.loc[id_ind]['id']) & (df['frame']>=re_ids.loc[id_ind]['frame']) & (label['id']=='teacher')
-                        if 'kid' in data['id'].map(str).unique():
-                            label.loc[matrix,'id']='neither'
-                        else:
-                            label.loc[matrix,'id']='kid'
-                label.loc[df['id']==label.iloc[ind]['id'],'id']='teacher'
-            elif order==ord('n'):
-                label.loc[df['id']==label.iloc[ind]['id'],'id']='neither'
-            elif order==ord('s'):
-                skip=True
-                continue
-            cv2.destroyAllWindows()
-    sl.write("Labelling done.")
-    sl.divider()
-    # sl.write(label)
-    # sl.write(fourcc,fps,height,width)
-    out=cv2.VideoWriter('out.mp4',fourcc,fps,(width,height))
-    for i in range(frame_num):
-        img=frame_list[i]
-        frame_ind=i+1
-        frame_anno=label[label['frame']==frame_ind]
-        for num in frame_anno.index:
-            [xm,ym,xM,yM]=[int(frame_anno['xmin'][num]*w),int(frame_anno['ymin'][num]*h),
-                           int(frame_anno['xmax'][num]*w),int(frame_anno['ymax'][num]*h)]
-            cv2.rectangle(img,
-                    (xm,ym),
-                    (xM,yM),
-                    (0,255,0),2)
-            cv2.putText(img,
-                '%s'%frame_anno['id'][num],
-                (xm,yM+25),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,(0,255,0),2)
-        out.write(img)
-    out.release()
-    sl.video('out.mp4', format='video/mp4', start_time=0)
+                    'frame:%d'%frame_ind,
+                    (0,50),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1.5,(0,0,255),3)
+            sl.write("Is the headbox with id:%d the kid,the teacher or neither?"%cur)
+            sl.image(img,channels="BGR")
+            col1,col2,col3=sl.columns(3)
+            kid=col1.button("Kid",key="kid%d"%cur,on_click=click,args=['kid',data,cur])
+            tea=col2.button("Teacher",key="tea%d"%cur,on_click=click,args=['tea',data,cur])
+            nei=col3.button("Neither",key="nei%d"%cur,on_click=click,args=['nei',data,cur])
+            # quit=col4.button("Quit",key="quit%d"%cur,on_click=click,args=['quit',data,cur])
+        else:
+            sl.write("Labelling done.")
+            sl.divider()
+            out=cv2.VideoWriter('out.mp4',sl.session_state.fourcc,sl.session_state.fps,(sl.session_state.width,
+                                                                                        sl.session_state.height))
+            for i in range(len(sl.session_state.frame_list)):
+                img=sl.session_state.frame_list[i]
+                w,h=img.shape[1],img.shape[0]
+                frame_ind=i+1
+                frame_anno=sl.session_state.label[sl.session_state.label['frame']==frame_ind]
+                for num in frame_anno.index:
+                    [xm,ym,xM,yM]=[int(frame_anno['xmin'][num]*w),int(frame_anno['ymin'][num]*h),
+                                int(frame_anno['xmax'][num]*w),int(frame_anno['ymax'][num]*h)]
+                    cv2.rectangle(img,
+                            (xm,ym),
+                            (xM,yM),
+                            (0,255,0),2)
+                    cv2.putText(img,
+                        '%s'%frame_anno['id'][num],
+                        (xm,yM+25),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,(0,255,0),2)
+                out.write(img)
+            out.release()
+            sl.video('out.mp4', format='video/mp4', start_time=0)
